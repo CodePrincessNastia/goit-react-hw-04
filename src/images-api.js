7dd67e588641fa314d877d1adcfe2a55c958b3b3
CodePrincessNastia <145288463+CodePrincessNastia@users.noreply.github.com>
2024-05-04 22:37:14 +0300

    add key

--- src/images-api.js
+++ src/images-api.js
@@ -1,12 +1,13 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = "https://api.unsplash.com/search/photos";
+
 export const searchImg = async (search, currentPage) => {
     try {
         const response = await axios.get("", {
             params: {
-                clients_id: "IsrfhvqbRkVjtUaZbmbOJhUkl5q7- L7tAt14Hn0jZa0",
-                 query : search,
+                client_id: "IsrfhvqbRkVjtUaZbmbOJhUkl5q7-L7tAt14Hn0jZa0",
+                query : search,
                 page : currentPage,
                 per_page: 12,
             },
