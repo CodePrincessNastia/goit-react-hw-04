03d12a89307e298d83ac694c94e3b24947a6ce6a
CodePrincessNastia <145288463+CodePrincessNastia@users.noreply.github.com>
2024-06-02 18:09:53 +0300

    add home work

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-import css from './ErrorMessage.module.css'
-import { FaRegSadCry } from "react-icons/fa";
-
-export default function ErrorMessage() {
-    return (
-        <p className={css.error}><FaRegSadCry className={css.icon} />
-            Sorry, something wrong. Try again!</p>
-    )
-}
